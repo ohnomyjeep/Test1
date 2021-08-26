eada0e429ec98a90959220a268a22348a29e95f8
ohnomyjeep <ohnomyjeep@gmail.com>
2021-08-26 15:07:18 +0300

    Test1

---
+++ 1. Условие/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner bat = new Scanner(System.in);
+        int b;
+        b = bat.nextInt();
+      if (b>7) {
+          System.out.print("Привет");
+      }
+    }
+}
